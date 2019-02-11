e0956701cb94a114c61d96921081690e45721088
Mikkel Madsen <mikkelmadsen@mikkels-air.home>
2019-02-11 19:25:17 +0100

    oprydning af kode

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,23 @@
 "use strict";
+document.addEventListener("DOMContentLoaded", init);
+
+let dest = document.querySelector("#typewriter");
+let text = document.querySelector("#typewriter").textContent;
+let counter = 0;
+
+function init() {
+  console.log("init");
+  dest.textContent = "";
+  loop();
+}
+
+function loop() {
+  console.log("loop");
+
+  if (text.length > counter) {
+    counter++;
+    let textLength = text.substring(0, counter);
+    dest.textContent = textLength;
+    setTimeout(loop, 400);
+  }
+}
